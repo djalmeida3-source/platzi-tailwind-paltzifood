3c0bd95379aabbc482f40715fb3ad31d28f4f2c9
David Almeida <djalmeida3@gmail.com>
2022-07-24 19:28:09 -0400

    Adding test excersices

--- postcss.config.js
+++ postcss.config.js
@@ -4,17 +4,17 @@ module.exports = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
-        purgecss({
-            content: ['./**/*.html',
-            // Para agregar soporte para otro tipo de archivos.
-            // './**/*.js',
-            // './**/*.vue'
-            ],
-            // IMPORTANTE: Para soportar pseudo-clases
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-        }),
-        require('cssnano')({
-            preset: 'default',
-        })
+        // purgecss({
+        //     content: ['./**/*.html',
+        //     // Para agregar soporte para otro tipo de archivos.
+        //     // './**/*.js',
+        //     // './**/*.vue'
+        //     ],
+        //     // IMPORTANTE: Para soportar pseudo-clases
+        //     defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+        // }),
+        // require('cssnano')({
+        //     preset: 'default',
+        // })
     ]
 }
